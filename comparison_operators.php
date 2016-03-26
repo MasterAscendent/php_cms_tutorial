06f496e3f676d22f55c9970a3226fa56b6edd203
Steven Hensley II <stevehensley2@gmail.com>
2016-03-25 20:41:08 -0700

    comparison_operators.php

---
+++ comparison_operators.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+
+  </head>
+  <body>
+
+    <!--
+
+      <h2>Comparison Operators</h2>
+
+      <pre>
+
+        equal ==
+        indentical ==
+        compare > < >= <= <>
+        not equal !=
+        not indentical !==
+
+      </pre>
+
+      <h2>Logical Operators</h2>
+      <pre>
+
+        And &&
+        Or ||
+        Not !
+
+      </pre>
+
+    -->
+
+    <?php
+
+      if(4 <= 4 || 5 < 10 ){
+
+        echo "it is  true";
+
+      };
+
+     ?>
+
+  </body>
+</html>
