eae4d2e0ee3ff206edcee2bfa59f21b4b72bc945
Steven Hensley II <stevehensley2@gmail.com>
2016-03-25 17:01:56 -0700

    Created some dynamic text with PHP, a css folder and styles.css and am working on comments in php
    Summary says it all. Created some dynamic text, a css folder and file
    and am working on comments in php presently.

---
+++ dynamic_text.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title></title>
+
+     <link rel="stylesheet" href="css/styles.css" />
+  </head>
+  <body>
+
+    <?php
+
+    $title = "OPEN SOURCE STUDIOS";
+
+     ?>
+
+     <h1><?php echo $title ?></h1>
+
+         <script src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
+         <script src="https://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.15.0/jquery.validate.min.js"></script>
+         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+
+
+  </body>
+</html>
