4f32145777208c8e3428cbeed533f5c6d9ebc547
Steven Hensley II <stevehensley2@gmail.com>
2016-03-25 18:18:10 -0700

    Associate arrays and fix blank.php again

---
+++ arrays_assoc.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+
+  </head>
+  <body>
+
+    <?php
+      $number = array('Steven', 'Paul', 'Bob');
+
+      // print_r($number);
+
+      // echo $number[2] . "<br />";
+      echo "<br />";
+
+      $names = array("first_name" => 'Hope', "last_name" => 'Phillips');
+
+      // print_r($names);
+
+      echo $names['first_name'] . " " . $names['last_name'];
+     ?>
+
+  </body>
+</html>
