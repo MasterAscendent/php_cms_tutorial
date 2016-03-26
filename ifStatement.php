57a840b4848591b334693d0ef070ed8c4bf952a7
Steven Hensley II <stevehensley2@gmail.com>
2016-03-25 20:25:06 -0700

    Added ifStatement.php

---
+++ ifStatement.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+
+  </head>
+  <body>
+
+    <?php
+      if (3 > 10){
+        echo "three is less than ten";
+      }elseif(4 < 5){
+        echo "of course four is less than five";
+      };
+
+     ?>
+
+  </body>
+</html>
