ee36136196b2c2573fcdb5489c3b3d2073d2294e
Steven Hensley II <stevehensley2@gmail.com>
2016-03-25 20:51:13 -0700

    Added switch_statement.php

---
+++ switch_statements.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+
+  </head>
+  <body>
+
+<?php
+
+    $number = 100;
+
+    switch($number){
+
+      case 34:
+      echo "it is 34";
+      break;
+      case 37:
+      echo "it is 34";
+      break;
+      case 35:
+      echo "it is 34";
+      break;
+      case 24:
+      echo "it is 34";
+      break;
+
+      default :
+      echo "we could not find anything";
+      break;
+    }
+
+?>
+
+  </body>
+</html>
