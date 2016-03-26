b57cc0006ab39b557f648b6c0d39687a90f0d26b
Steven Hensley II <stevehensley2@gmail.com>
2016-03-25 17:57:44 -0700

    Created math.php to play with it and deleted a line in blank.php

--- blank.php
+++ blank.php
@@ -8,7 +8,7 @@
   <body>
 
     <?php
-      
+
      ?>
   </body>
 </html>
---
+++ math.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+
+  </head>
+  <body>
+
+    <?php
+      echo 56 + 45;
+      echo "<br />"
+      echo 56 + 45;
+      echo "<br />"
+      echo 56 + 45;
+      echo "<br />"
+      echo 56 + 45;
+      echo "<br />"
+      echo "<br />"
+
+      echo 45 + 34 * 45 /421 - 45;
+      echo "<br />"
+      echo (5+5) * 10;
+     ?>
+  </body>
+</html>
