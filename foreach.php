aacb8d4c80e096d94820c5cf22e2f9b8ebb27045
Steven Hensley II <stevehensley2@gmail.com>
2016-03-25 21:54:48 -0700

    Foreach.php

---
+++ foreach.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+
+  </head>
+  <body>
+
+    <?php
+
+    $numbers = array(345,347,676,385,2657,5784);
+
+      foreach($numbers as $number){
+        echo $number . "<br />";
+      }
+
+     ?>
+
+  </body>
+</html>
