241a5557d9f9b280df6006bd1b59ef193aa24a45
Steven Hensley II <stevehensley2@gmail.com>
2016-03-26 19:16:56 -0700

    Constants.php

---
+++ constants.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+
+  </head>
+  <body>
+
+    <?php
+
+    $number = 10;
+    $number = 1000;
+
+    $number = "Steve";
+    echo $number . "<br />";
+
+    define("NAME", 1000);
+
+    echo NAME;
+
+
+     ?>
+
+  </body>
+</html>
