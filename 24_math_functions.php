5c63a0903f4692444096e33ed213a9e1dd8092ce
Steven Hensley II <stevehensley2@gmail.com>
2016-03-26 20:32:38 -0700

    24_math_functions.php from tutorial

---
+++ 24_math_functions.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+
+  </head>
+  <body>
+
+    <?php
+
+    echo pow(2,7);
+
+    echo "<br />";
+
+    echo rand(1, 1000);
+
+    echo "<br />";
+
+    echo sqrt(100);
+
+    echo "<br />";
+
+    echo ceil(4.6);
+
+    echo "<br />";
+
+    echo floor(4.6);
+
+    echo "<br />";
+
+    echo round(4.5);
+
+     ?>
+
+  </body>
+</html>
