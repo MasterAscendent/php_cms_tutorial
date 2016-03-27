1f2f6670428dff50050f4994d63d0f97fbe51004
Steven Hensley II <stevehensley2@gmail.com>
2016-03-26 20:36:37 -0700

    string_functions

---
+++ 27_string_functions.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+
+  </head>
+  <body>
+
+    <?php
+      $string = "Hello student do you like the class?";
+
+      echo strlen($string);
+
+      echo "<br />"
+
+      echo strtoupper($string);
+
+      echo "<br />"
+
+      echo strtolower($string);
+
+     ?>
+
+  </body>
+</html>
