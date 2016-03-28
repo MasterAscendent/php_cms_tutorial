774a3c0ecae833c86dd72021cf3102436638c8cf
Steven Hensley II <stevehensley2@gmail.com>
2016-03-28 16:49:17 -0700

    Found the problem.
    It had be the fact that WAMP was installed I think. :)

--- tutorial_stuff/mysql/login_create.php
+++ tutorial_stuff/mysql/login_create.php
@@ -21,7 +21,7 @@
       $result = mysqli_query($connection, $query);
 
       if(!$result){
-        die('Query FAILED' . mysqli_error());
+        die('Query FAILED' . mysqli_error($connection));
       }
 
 
