b819b4918f46e47c4098903a0a91be37bda11ade
Steven Hensley II <stevehensley2@gmail.com>
2016-03-29 13:40:19 -0700

    Tutorial #54. SQL Injections - How to prevent it

--- tutorial_stuff/mysql/functions.php
+++ tutorial_stuff/mysql/functions.php
@@ -4,9 +4,14 @@
 function createRows(){
 
   if(isset($_POST['submit'])){
+
       global $connection;
+
       $username = $_POST['username'];
       $password = $_POST['password'];
+
+      $username = mysqli_real_escape_string($connection, $username);
+      $password = mysqli_real_escape_string($connection, $password);
 
       $query = "INSERT INTO users(username,password) ";
       $query .= "VALUES ('$username', '$password')";
@@ -22,9 +27,12 @@ function createRows(){
 }
 
 function readRows() {
+
   global $connection;
+
   $query = "SELECT * FROM users";
   $result = mysqli_query($connection, $query);
+
   if(!$result){
     die('Query FAILED' . mysqli_error($connection));
   }
@@ -34,26 +42,32 @@ function readRows() {
 }
 
 function showAllData(){
+
     global $connection;
+
     $query = "SELECT * FROM users";
     $result = mysqli_query($connection, $query);
+
     if(!$result){
       die('Query FAILED' . mysqli_error($connection));
     }
 
     while($row = mysqli_fetch_assoc($result)){
       $id = $row['id'];
-
       echo "<option value='$id'>$id</option>";
     }
 
 }
 
 function UpdateTable() {
+
   if(isset($_POST['submit'])) {
+
     global $connection;
+
     $username = $_POST['username'];
     $password = $_POST['password'];
+
     $id = $_POST['id'];
 
     $query = "UPDATE users SET ";
@@ -62,6 +76,7 @@ function UpdateTable() {
     $query .= "WHERE id = $id ";
 
     $result = mysqli_query($connection, $query);
+
     if(!$result){
       die("QUERY FAILED" . mysqli_error($connection));
     } else {
@@ -71,16 +86,21 @@ function UpdateTable() {
 }
 
   function deleteRows() {
+
     if(isset($_POST['submit'])) {
+
       global $connection;
+
       $username = $_POST['username'];
       $password = $_POST['password'];
+
       $id = $_POST['id'];
 
       $query = "DELETE FROM users ";
       $query .= "WHERE id = $id ";
 
       $result = mysqli_query($connection, $query);
+
       if(!$result){
         die("QUERY FAILED" . mysqli_error($connection));
       } else {
