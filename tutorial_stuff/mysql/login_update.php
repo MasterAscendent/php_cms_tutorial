2805fb7fbd708ec80f0b51ac3e298c25f112aced
Steven Hensley II <stevehensley2@gmail.com>
2016-03-28 18:39:43 -0700

    Tutorial #45
    Added pages db.php and login_update.php from tutorial #45.

---
+++ tutorial_stuff/mysql/login_update.php
@@ -0,0 +1,53 @@
+<?php include "db.php";
+      $query = "SELECT * FROM users";
+      $result = mysqli_query($connection, $query);
+      if(!$result){
+        die('Query FAILED' . mysqli_error($connection));
+      }
+
+ ?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+  </head>
+  <body>
+
+    <div class="container">
+
+      <div class="col-sm-6">
+
+        <form action="login_create.php" method="post">
+
+          <div class="form-group">
+            <label for="username">Username</label>
+            <input type="text" name="username" class="form-control">
+          </div>
+
+          <div class="form-group">
+            <lable for="password">Password</lable>
+            <input type="password" name="password" class="form-control" />
+          </div>
+
+          <div class="form-group">
+
+            <select name="" id="">
+
+              <option value="">1</option>
+            </select>
+
+          </div>
+
+          <input class="btn btn-primary" type="submit" name="submit" value="UPDATE">
+
+        </form>
+
+      </div>
+    </div>
+  </body>
+</html>
