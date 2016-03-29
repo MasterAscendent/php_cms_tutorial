a3c50128bcf36ce69976e4f565cd1e1c4a7fc0ac
Steven Hensley II <stevehensley2@gmail.com>
2016-03-28 20:06:23 -0700

    Finished tutorial #49.

--- tutorial_stuff/mysql/functions.php
+++ tutorial_stuff/mysql/functions.php
@@ -17,4 +17,20 @@ function showAllData(){
 
 }
 
+function UpdateTable() {
+    global $connection;
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $id = $_POST['id'];
+
+    $query = "UPDATE users SET ";
+    $query .= "username = '$username', ";
+    $query .= "password = '$password' ";
+    $query .= "WHERE id = $id ";
+
+    $result = mysqli_query($connection, $query);
+    if(!$result){
+      die("QUERY FAILED" . mysqli_error($connection));
+    }
+  }
 ?>
--- tutorial_stuff/mysql/login_update.php
+++ tutorial_stuff/mysql/login_update.php
@@ -3,21 +3,7 @@
 
 <?php
 if(isset($_POST['submit'])) {
-
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-    $id = $_POST['id'];
-
-$query = "UPDATE users SET ";
-$query .= "username = '$username', ";
-$query .= "password = '$password' ";
-$query .= "WHERE id = $id ";
-
-    $result = mysqli_query($connection, $query);
-    if(!$result){
-      die("QUERY FAILED" . mysqli_error($connection));
-    }
-
+    UpdateTable();
 }
 
 ?>
