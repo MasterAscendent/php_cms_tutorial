a34671377bfd5e1424d7a1b6caed00e46318f612
Steven Hensley II <stevehensley2@gmail.com>
2016-03-29 18:23:56 -0700

    #62. Reading Cookies in PHP

--- tutorial_stuff/cookies.php
+++ tutorial_stuff/cookies.php
@@ -1,3 +1,14 @@
+<?php
+
+$name = "SomeName";
+$value = 100;
+$expiration = time() + (60*60*24*7);
+
+setcookie($name,$value,$expiration);
+
+
+ ?>
+
 <!DOCTYPE html>
 <html>
   <head>
@@ -9,7 +20,18 @@
 
     <?php
 
+      if(isset($_COOKIE["SomeName"])) {
+
+        $someOne = $_COOKIE["SomeName"];
+        echo $someOne;
+
+      } else {
+
+          $someOne = "";
+
+      }
+
      ?>
-     
+
   </body>
 </html>
