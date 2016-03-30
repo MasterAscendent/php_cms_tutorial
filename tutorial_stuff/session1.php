edebb0dedb004d8dd068cb293ab14bef5a4810b7
Steven Hensley II <stevehensley2@gmail.com>
2016-03-29 18:47:08 -0700

    #63. How to use Sessions in PHP

---
+++ tutorial_stuff/session1.php
@@ -0,0 +1,15 @@
+<?php session_start();
+  $_SESSION['greeting'] = "Hello Student";
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+
+  </head>
+  <body>
+
+  </body>
+</html>
---
+++ tutorial_stuff/session2.php
@@ -0,0 +1,15 @@
+<?php session_start();
+  echo $_SESSION['greeting'];
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+
+  </head>
+  <body>
+
+  </body>
+</html>
