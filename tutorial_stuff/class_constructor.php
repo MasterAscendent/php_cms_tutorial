84c3f54d8c5a2a1f5dbf2e40b365bce4a98381af
Steven Hensley II <stevehensley2@gmail.com>
2016-03-30 08:01:25 -0700

    #70. Constructors

---
+++ tutorial_stuff/class_constructor.php
@@ -0,0 +1,22 @@
+<?php
+
+  class Car {
+
+    var $Wheels = 4;
+    var $hood = 1;
+    var $engine = 1;
+    var $doors = 4;
+
+    function __construct(){
+
+      echo $this->wheels = 10;
+
+    }
+}
+    $bmw = new Car();
+
+    $truck = new Car();
+
+    $semi = new Car();
+    
+ ?>
